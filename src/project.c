b870411a0f06f5e84060811d52d4cb9e1de398be
Markus Murto <markus@murto.eu>
2022-04-18 13:31:04 +0300

    Run clang formatter

--- src/project.c
+++ src/project.c
@@ -31,8 +31,10 @@
  */
 int compare_int(int a, int b)
 {
-	if (a < b) return -1;
-	if (a > b) return 1;
+	if (a < b)
+		return -1;
+	if (a > b)
+		return 1;
 	return 0;
 }
 
@@ -50,14 +52,16 @@ int compare_int(int a, int b)
  */
 int compare_meeting(const void *meeting0, const void *meeting1)
 {
-	Meeting *a = (Meeting*) meeting0;
-	Meeting *b = (Meeting*) meeting1;
+	Meeting *a = (Meeting *)meeting0;
+	Meeting *b = (Meeting *)meeting1;
 
 	int months = compare_int(a->date.month, b->date.month);
-	if (months != 0) return months;
+	if (months != 0)
+		return months;
 
 	int days = compare_int(a->date.day, b->date.day);
-	if (days != 0) return days;
+	if (days != 0)
+		return days;
 
 	return compare_int(a->date.hour, b->date.hour);
 }
@@ -79,13 +83,11 @@ int compare_meeting(const void *meeting0, const void *meeting1)
  */
 void print_calendar(Meeting *calendar, int num)
 {
-	qsort((void*) calendar, num, sizeof(Meeting), compare_meeting);
+	qsort((void *)calendar, num, sizeof(Meeting), compare_meeting);
 	for (int i = 0; i < num; i++) {
-		printf("%s %02d.%02d at %02d\n",
-				calendar[i].description,
-				calendar[i].date.day,
-				calendar[i].date.month,
-				calendar[i].date.hour);
+		printf("%s %02d.%02d at %02d\n", calendar[i].description,
+		       calendar[i].date.day, calendar[i].date.month,
+		       calendar[i].date.hour);
 	}
 }
 
@@ -106,10 +108,11 @@ void print_calendar(Meeting *calendar, int num)
  * \return `int` Index of matching meeting timeslot in calendar, `-1` if there is
  * no match
  */
-int check_timeslot(Meeting* calendar, int num, Meeting newmeeting)
+int check_timeslot(Meeting *calendar, int num, Meeting newmeeting)
 {
 	for (int i = 0; i < num; i++) {
-		if (compare_meeting((void*) &calendar[i], (void*) &newmeeting) == 0) {
+		if (compare_meeting((void *)&calendar[i],
+				    (void *)&newmeeting) == 0) {
 			return i;
 		}
 	}
@@ -137,7 +140,7 @@ Meeting *add_meeting(Meeting *calendar, int num, Meeting newmeeting)
 	if (check_timeslot(calendar, num, newmeeting) != -1)
 		return NULL;
 
-	calendar = (Meeting*) realloc(calendar, (num + 1) * sizeof(Meeting));
+	calendar = (Meeting *)realloc(calendar, (num + 1) * sizeof(Meeting));
 	if (!calendar) {
 		printf("ERROR: Could not allocate memory for new calendar!\n");
 		exit(1);
@@ -173,7 +176,7 @@ Meeting *delete_meeting(Meeting *calendar, int num, MeetingDate timeslot)
 	if (del_i == -1)
 		return NULL;
 
-	Meeting *newcalendar = (Meeting*) calloc(num - 1, sizeof(Meeting));
+	Meeting *newcalendar = (Meeting *)calloc(num - 1, sizeof(Meeting));
 	if (!newcalendar) {
 		printf("ERROR: Could not allocate memory for new calendar!\n");
 		exit(1);
@@ -218,10 +221,8 @@ int write_calendar(Meeting *calendar, int num, const char *filename)
 	memset(buffer, '\0', 1000);
 	for (int i = 0; i < num; i++) {
 		snprintf(buffer, 1000, "%s %02d.%02d at %02d\n",
-				calendar[i].description,
-				calendar[i].date.day,
-				calendar[i].date.month,
-				calendar[i].date.hour);
+			 calendar[i].description, calendar[i].date.day,
+			 calendar[i].date.month, calendar[i].date.hour);
 		fputs(buffer, file_ptr);
 	}
 	if (ferror(file_ptr)) {
@@ -298,7 +299,7 @@ Command command_parser()
 	command.meetingdate.month = 0;
 	command.meetingdate.day = 0;
 	command.meetingdate.hour = 0;
-	command.message = (char*) malloc(sizeof(char));
+	command.message = (char *)malloc(sizeof(char));
 	if (!command.message) {
 		printf("ERROR: Could not allocate memory for new command message!\n");
 		exit(1);
@@ -308,18 +309,12 @@ Command command_parser()
 	char type[64], param0[64], param1[64], param2[64], param3[64];
 	char line[1000];
 	fgets(line, 1000, stdin);
-	sscanf(line, "%s %s %s %s %s",
-			type,
-			param0,
-			param1,
-			param2,
-			param3);
+	sscanf(line, "%s %s %s %s %s", type, param0, param1, param2, param3);
 
 	if (strcmp(type, "A") == 0) {
 		command.type = A;
-		command.message = (char*) realloc(
-				command.message,
-				(strlen(param0) + 1)* sizeof(char));
+		command.message = (char *)realloc(
+			command.message, (strlen(param0) + 1) * sizeof(char));
 		if (!command.message) {
 			printf("ERROR: Could not allocate memory for new command message!\n");
 			exit(1);
@@ -339,8 +334,8 @@ Command command_parser()
 	}
 	if (strcmp(type, "W") == 0) {
 		command.type = W;
-		command.message = (char*) realloc(command.message,
-				(strlen(param0) + 1) * sizeof(char));
+		command.message = (char *)realloc(
+			command.message, (strlen(param0) + 1) * sizeof(char));
 		if (!command.message) {
 			printf("ERROR: Could not allocate memory for new command message!\n");
 			exit(1);
@@ -350,7 +345,8 @@ Command command_parser()
 	}
 	if (strcmp(type, "O") == 0) {
 		command.type = O;
-		command.message = (char*) realloc(command.message, strlen(param0) * sizeof(char));
+		command.message = (char *)realloc(
+			command.message, strlen(param0) * sizeof(char));
 		if (!command.message) {
 			printf("ERROR: Could not allocate memory for new command message!\n");
 			exit(1);
@@ -383,12 +379,12 @@ Command command_parser()
  */
 int main()
 {
-	Meeting *calendar = (Meeting*) calloc(2, sizeof(Meeting));
+	Meeting *calendar = (Meeting *)calloc(2, sizeof(Meeting));
 	if (!calendar) {
 		printf("Error: failed to alloc for calendar!");
 		return 1;
 	}
-	
+
 	Command command;
 	Meeting *processed;
 	Meeting newmeeting;
@@ -396,62 +392,63 @@ int main()
 	do {
 		command = command_parser();
 		switch (command.type) {
-			case A:
-				newmeeting.date = command.meetingdate;
-				memset(newmeeting.description, '\0', 64);
-				strcpy(newmeeting.description, command.message);
-				processed = add_meeting(calendar, num, newmeeting);
-				if (!processed) {
-					printf("ERROR: Meeting timeslot already taken!\n");
-					free(command.message);
-					break;
-				}
-				num++;
-				printf("SUCCESS\n");
-				calendar = processed;
+		case A:
+			newmeeting.date = command.meetingdate;
+			memset(newmeeting.description, '\0', 64);
+			strcpy(newmeeting.description, command.message);
+			processed = add_meeting(calendar, num, newmeeting);
+			if (!processed) {
+				printf("ERROR: Meeting timeslot already taken!\n");
 				free(command.message);
 				break;
-			case D:
-				processed = delete_meeting(calendar, num, command.meetingdate);
-				if (!processed) {
-					printf("ERROR: Could not delete a meeting, such meeting does not exist\n");
-					free(command.message);
-					break;
-				}
-				num--;
-				printf("SUCCESS\n");
-				calendar = processed;
+			}
+			num++;
+			printf("SUCCESS\n");
+			calendar = processed;
+			free(command.message);
+			break;
+		case D:
+			processed = delete_meeting(calendar, num,
+						   command.meetingdate);
+			if (!processed) {
+				printf("ERROR: Could not delete a meeting, such meeting does not exist\n");
 				free(command.message);
 				break;
-			case L: 
-				print_calendar(calendar, num);
-				printf("SUCCESS\n");
+			}
+			num--;
+			printf("SUCCESS\n");
+			calendar = processed;
+			free(command.message);
+			break;
+		case L:
+			print_calendar(calendar, num);
+			printf("SUCCESS\n");
+			free(command.message);
+			break;
+		case W:
+			if (write_calendar(calendar, num, command.message)) {
+				printf("ERROR: Error while writing a file.\n");
 				free(command.message);
 				break;
-			case W:
-				if (write_calendar(calendar, num, command.message)) {
-					printf("ERROR: Error while writing a file.\n");
-					free(command.message);
-					break;
-				}
-				printf("SUCCESS\n");
-				free(command.message);
-				break;
-			case O:
-				free(command.message);
-				break;
-			case Q:
-				printf("SUCCESS\n");
-				free(command.message);
-				break;
-			case ERROR:
-				printf("ERROR: Could not process command, please try again.\n");
-				free(command.message);
-				break;
-			default:
-				printf("ERROR: something went horribly wrong!\n");
-				free(command.message);
-				break;
+			}
+			printf("SUCCESS\n");
+			free(command.message);
+			break;
+		case O:
+			free(command.message);
+			break;
+		case Q:
+			printf("SUCCESS\n");
+			free(command.message);
+			break;
+		case ERROR:
+			printf("ERROR: Could not process command, please try again.\n");
+			free(command.message);
+			break;
+		default:
+			printf("ERROR: something went horribly wrong!\n");
+			free(command.message);
+			break;
 		}
 	} while (command.type != Q);
 
